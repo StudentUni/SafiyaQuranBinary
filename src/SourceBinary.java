4afd5d28e3a940295d40a23320058117e6d72c5d
StudentUni <57175112+StudentUni@users.noreply.github.com>
2020-03-28 13:23:20 +0200

    Update SourceBinary.java

--- src/SourceBinary.java
+++ src/SourceBinary.java
@@ -79,6 +79,38 @@ public class SourceBinary {
                 }
 
             System.out.println("result2  = " + result);
+                
+                
+                int[] arr_num=new int[]{7,286,200,176,120,165,206,75,129,109,123,111,43,52,99,128,
+                  111,110,98,135,112,78,118,64,77,227,93,88,69,60,34,30,73,54,45,83,182,88,75,85,
+                  54,53,89,59,37,35,38,29,18,45,60,49,62,55,78,96,29,22,24,13,14,11,11,18,12,12,
+                  30,52,52,44,28,28,20,56,40,31,50,40,46,42,29,19,36,25,22,17,19,26,30,20,15,21
+                      ,11,8,8,19,5,8,8,11,11,8,3,9,5,4,7,3,6,3,5,4,5,6};  
+          
+    
+              
+             int number=0,result_number=0,aya=0,c=0;
+             for(int i=0;i<arr_num.length;i++){
+                 number+=arr_num[i];
+                 if(i==0){
+                     
+                 }else{
+                c+=arr_num[(i-1)];
+                 }
+                 
+                  if(result<=number){  
+              
+                      result_number=i;
+                     aya=result-c;
+                   
+                    break;
+                 }
+             }  
+              
+            
+        
+                System.out.println("aya "+aya + " number of surah = "+(result_number+1));
+               
 
             } catch (Exception ex)
                 {
